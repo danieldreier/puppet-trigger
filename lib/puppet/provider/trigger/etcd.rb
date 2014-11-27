be5cfc4f2e52049b3ce799f1894c41b838d26cbe
Daniel Dreier <d@puppetlabs.com>
2014-11-27 13:04:25 -0800

    Add UUID to etcd provider

--- lib/puppet/provider/trigger/etcd.rb
+++ lib/puppet/provider/trigger/etcd.rb
@@ -1,4 +1,6 @@
 require 'json'
+require 'securerandom'
+
 Puppet::Type.type(:trigger).provide(:etcd) do
   commands :etcdctl => "etcdctl"
 
@@ -7,12 +9,17 @@ Puppet::Type.type(:trigger).provide(:etcd) do
     @hosts     = @resource[:hosts] if @resource[:hosts]
     @keyprefix = @resource[:keyprefix] if @resource[:keyprefix]
     @keyname   = @resource[:name]
+
+    # UUID provided to allow the client-side to avoid double-processing in case
+    # etcd delivers a message twice
+    @uuid      = SecureRandom.uuid
+
     @keypath   = "#{@keyprefix}/#{@keyname}"
     messages = {'hostname'  => Facter.value(:hostname),
-                'action'    => 'restart',
                 'tags'      => @tags,
                 'hosts'     => @hosts,
-                'timestamp' => Time.now.to_i }
+                'timestamp' => Time.now.to_i,
+                'uuid'      => @uuid }
     etcdctl('set', '--ttl', '180', @keypath.to_s, messages.to_json)
     @message = "restart triggered"
   end
